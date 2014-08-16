df4fee9a0721476ffa7840c5fd11469239aa37d5
Mark van Duijker <mark@duyker.nl>
2014-08-16 21:37:07 +0200

    Added tests for installing admin module

--- tests/JoomlaCli/Test/Console/Joomla/Extension/Installer/Adapter/ModuleTest.php
+++ tests/JoomlaCli/Test/Console/Joomla/Extension/Installer/Adapter/ModuleTest.php
@@ -8,19 +8,21 @@ use JoomlaCli\Console\Joomla\Extension\Installer;
 class ModuleTest extends \PHPUnit_Framework_TestCase
 {
 
-    protected $extensionPath;
+    protected $basePath;
 
     public function setUp()
     {
-        $basePath = __DIR__ . '/../../../../../../../resources/extensions/';
-        $this->extensionPath = realpath($basePath . 'module');
-        $this->manifest = new \SimpleXMLElement(file_get_contents($this->extensionPath . '/mod_test.xml'));
+        $this->basePath = __DIR__ . '/../../../../../../../resources/extensions/';
+
         $this->target = sys_get_temp_dir() . '/joomla-cli-unit-test';
     }
 
     public function testInstall()
     {
-        $adapter = new Installer\Adapter\Module($this->extensionPath, $this->manifest);
+        $extensionPath = realpath($this->basePath . 'module');
+        $manifest = new \SimpleXMLElement(file_get_contents($extensionPath . '/mod_test.xml'));
+
+        $adapter = new Installer\Adapter\Module($extensionPath, $manifest);
         $adapter->install($this->target);
 
         $this->assertTrue(is_dir($this->target  . '/modules/mod_test'));
@@ -28,6 +30,22 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(file_exists($this->target . '/language/en-GB/en-GB.mod_test.ini'));
         $this->assertTrue(file_exists($this->target . '/modules/mod_test/mod_test.php'));
         $this->assertTrue(file_exists($this->target . '/modules/mod_test/test.scriptfile.php'));
+        $this->assertTrue(file_exists($this->target . '/media/mod_test/test.png'));
+    }
+
+    public function testInstallAdmin()
+    {
+        $extensionPath = realpath($this->basePath . 'admin-module');
+        $manifest = new \SimpleXMLElement(file_get_contents($extensionPath . '/mod_test.xml'));
+
+        $adapter = new Installer\Adapter\Module($extensionPath, $manifest);
+        $adapter->install($this->target);
+
+        $this->assertTrue(is_dir($this->target  . '/administrator/modules/mod_test'));
+        $this->assertTrue(file_exists($this->target . '/administrator/modules/mod_test/mod_test.xml'));
+        $this->assertTrue(file_exists($this->target . '/administrator/language/en-GB/en-GB.mod_test.ini'));
+        $this->assertTrue(file_exists($this->target . '/administrator/modules/mod_test/mod_test.php'));
+        $this->assertTrue(file_exists($this->target . '/administrator/modules/mod_test/test.scriptfile.php'));
         $this->assertTrue(file_exists($this->target . '/media/mod_test/test.png'));
     }
 
